a785fefc50c6d40e2cf5aa3c118a023ea00468e6
ggatewood99 <greg.e.gatewood@outlook.com>
2024-01-08 17:17:01 +0000

    reading in data

---
+++ 99bottles.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+
+for x in range(99, 0,):
+
+    if bottles_of_beer == 0:
+        print("no more bottles of beer on the wall!")
+
+    else :
+        print(x, "bottles of beer on the wall!\n",x, "bottles of beer! You take one down, pass it around!")
+main()
